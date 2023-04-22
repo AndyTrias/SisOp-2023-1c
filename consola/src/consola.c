cabd202c8caad7d8474166a9e1e1932234b150dd
AndyTrias <andrestrias03@gmail.com>
2023-04-22 00:34:04 +0000

    hago pull

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -2,18 +2,34 @@
 
 int main(int argc, char *argv[]) {
 
-  if (argc != 2) {
-    printf("Debe ingresar el archivo de configuracion\n");
-    exit(1);
+  t_log* logger = iniciar_logger("./logs/consola.log", "CONSOLA");
+  if (argc < 3) {
+    
+    if (argc < 2)
+      log_error(logger, "Debe ingresar el archivo de configuracion\n");
+    else
+      log_error(logger, "Debe ingresar el archivo con las instrucciones\n");
+    
+    return EXIT_FAILURE;
   }
 
   
-  t_log* logger = iniciar_logger("./logs/consola.log", "CONSOLA");
   t_config* config = iniciar_config(argv[1]);
   
   int conexion_kernel;
   inicializar_conexiones(&conexion_kernel, config, logger);
 
+  char buffer[100];
+  FILE *f = fopen(argv[2], "r");
+  if (f == NULL) {
+    log_error(logger, "No se pudo abrir el archivo de instrucciones");
+    return EXIT_FAILURE;
+  }
+
+  while ((fgets(buffer, 100, f)) != NULL){
+    log_info(logger, "Instruccion: %s", buffer);
+  }
+  
   enviar_mensaje("Hola Kernel, soy la consola", conexion_kernel);
 
   terminar_conexiones(1, conexion_kernel);
