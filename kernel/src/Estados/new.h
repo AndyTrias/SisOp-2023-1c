6e3012aa69fff2f9ec9478cb6c41c7d06e814147
manutorrente <manutorrente9@gmail.com>
2023-04-23 01:37:46 +0000

    estructura ready

---
+++ kernel/src/Estados/new.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/collections/list.h>
+
+t_list *init_new();
+
+void nuevo_proceso(t_list *estado, instrucciones *proceso);
+
+t_pcb *get_proceso_desde_new(t_list *estado);
+
+
+
---
+++ kernel/src/Estados/ready.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/collections/list.h>
+#include <new.h>
+
+t_list *init_ready();
+
+bool hay_espacio_ready(t_list *estado);
+
+int grado_de_multiprogramacion(t_list *ready, int procesos_en_blocked);
+
+void admitir_proceso(t_list *ready, t_list *new);
+
+pcb *ceder_proceso_a_cpu(t_list *ready);
+
+
+
+
