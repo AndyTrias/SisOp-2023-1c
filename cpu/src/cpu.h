386529898a6339735e69a217fe9154e920e1105e
KevinAla <kevinalavera@frba.utn.edu.ar>
2023-05-04 18:25:47 +0000

    Para MAIN

--- cpu/src/cpu.h
+++ cpu/src/cpu.h
@@ -8,5 +8,6 @@
     #include <init.h>
     #include <stdio.h>
     #include <stdlib.h>
+    #include <instruccionescpu.h>
 
 #endif
---
+++ cpu/src/instruccionescpu.h
@@ -0,0 +1,14 @@
+
+#ifndef INSTRUCCIONESCPU_H_
+#define INSTRUCCIONESCPU_H_
+
+#include <utils/conexiones.h>
+#include <utils/planificacion/pcb.h>
+#include <utils/mensajes.h>
+#include <unistd.h>
+// #include <math.h>
+
+void ciclo_de_instruccion(t_ctx* ctx);
+
+
+#endif
