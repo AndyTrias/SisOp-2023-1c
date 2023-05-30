f7019bcb1b75b2f8b1a6d9d894fbb62fc7eba864
KevinAla <kevinalavera@frba.utn.edu.ar>
2023-05-30 01:41:07 +0000

    Termino IO

--- kernel/src/Estados/blocked.c
+++ kernel/src/Estados/blocked.c
@@ -1,21 +1,21 @@
 #include "blocked.h"
 // tiene que esperar todos los signal de los semaforos en SEMAFOROS_RECURSOS
 
+pthread_t hacer_IO;
 
 void wait(t_pcb *proceso)
 {
     int recurso_id = get_id_recurso(proceso->contexto.motivos_desalojo->parametros[0]);
     if (INSTANCIAS_RECURSOS[recurso_id] == 0)
     { // no hay recursos disponibles para darle
-        log_info(LOGGER_KERNEL, "Se recibio un mensaje de wait");
         reemplazar_exec_por_nuevo();
         list_add(list_get(LISTAS_BLOCK, recurso_id), proceso);
     }
     else
     {
         // sem_wait(SEMAFOROS_RECURSOS[recurso_id]);
-        log_info(LOGGER_KERNEL, "Se recibio un mensaje de wait del recurso %d", recurso_id);
         INSTANCIAS_RECURSOS[recurso_id]--;
+        log_info(LOGGER_KERNEL,"PID: <%d> - Wait: <%s> - Instancias: <%d>",proceso->contexto.PID,RECURSOS[recurso_id],atoi(INSTANCIAS_RECURSOS[recurso_id]));
     };
 }
 
@@ -33,26 +33,28 @@ void signal(t_pcb *proceso)
         // sem_signal(SEMAFOROS_RECURSOS[recurso_id]);
         INSTANCIAS_RECURSOS[recurso_id]++;
     }
-    log_info(LOGGER_KERNEL, "Se recibio un mensaje de signal del recurso %d", recurso_id);
+    log_info(LOGGER_KERNEL,"PID: <%d> - Signal: <%s> - Instancias: <%d>",proceso->contexto.PID,RECURSOS[recurso_id],atoi(INSTANCIAS_RECURSOS[recurso_id]));
 
 }
 
-void* algo_en_execute()
-{
-    reemplazar_exec_por_nuevo();
-    log_info(LOGGER_KERNEL, "TERMINO ESTA COSA");
+void* instruccion_IO(t_pcb * proceso)
+{  
+    t_instruccion* instruccion_utilizable = list_get(proceso->contexto.instrucciones, proceso->contexto.program_counter -1);
+    log_info(LOGGER_KERNEL, "PID: %d - Ejecuta IO: %s", proceso->contexto.PID, instruccion_utilizable->parametros[0]);
+    int tiempo = atoi(instruccion_utilizable->parametros[0]);
+    log_info(LOGGER_KERNEL, "DORMIR");
+    usleep(tiempo * 1000);
+    log_info(LOGGER_KERNEL, "LISTO ");
+    agregar_a_lista_ready(proceso);
     return NULL;
 }
 
-void io(t_pcb *proceso){
-    t_instruccion* instruccion_utilizable = list_get(proceso->contexto.instrucciones, proceso->contexto.program_counter -1);
-    int tiempo_io = atoi(instruccion_utilizable->parametros[0]); 
-    log_info(LOGGER_KERNEL, "PID: %d - Ejecuta IO: %d", proceso->contexto.PID, tiempo_io);
-    list_add(LISTAS_BLOCK, proceso);
-    pthread_t algo_en_ready;
-    pthread_create(&algo_en_ready, NULL, (void *) algo_en_execute, NULL);
-    usleep(tiempo_io * 1000);
-    t_pcb *proceso_a_desbloquear = list_remove(LISTAS_BLOCK, sizeof(RECURSOS));
-    agregar_a_lista_ready(proceso_a_desbloquear);
-    pthread_join(algo_en_ready, NULL);  
+void io(t_pcb *proceso)
+{
+    pthread_create(&hacer_IO, NULL, (void *) instruccion_IO, proceso);
+    pthread_detach(hacer_IO);
+    
+    reemplazar_exec_por_nuevo();
+    log_info(LOGGER_KERNEL, "TERMINO ESTA COSA");
 }
+
