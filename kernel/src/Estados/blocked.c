1b1207f770f77b6bd6dbe839bc2beebfb0e1813c
JuanFrancisco9 <jucaceres@frba.utn.edu.ar>
2023-06-03 01:57:10 +0000

    agrego log para recurso inexistente

--- kernel/src/Estados/blocked.c
+++ kernel/src/Estados/blocked.c
@@ -6,36 +6,45 @@ pthread_t hacer_IO;
 void wait(t_pcb *proceso)
 {
     int recurso_id = get_id_recurso(proceso->contexto.motivos_desalojo->parametros[0]);
-    if (INSTANCIAS_RECURSOS[recurso_id] == 0)
-    { // no hay recursos disponibles para darle
+    if(recurso_id != -1){
+        if (INSTANCIAS_RECURSOS[recurso_id] == 0){ // no hay recursos disponibles para darle
         reemplazar_exec_por_nuevo();
         cambio_de_estado(proceso->contexto.PID,"Exec","Block");
         list_add(list_get(LISTAS_BLOCK, recurso_id), proceso);
-    }
-    else
-    {
+        }
+     else{
         // sem_wait(SEMAFOROS_RECURSOS[recurso_id]);
         INSTANCIAS_RECURSOS[recurso_id]--;
-    };
+        }
     log_info(LOGGER_KERNEL,"PID: <%d> - Wait: <%s> - Instancias: <%d>",proceso->contexto.PID,RECURSOS[recurso_id],atoi(INSTANCIAS_RECURSOS[recurso_id]));
+    }else{
+        log_info(LOGGER_KERNEL,"Se realiza Wait de proceso inexitente: <%s>",proceso->contexto.motivos_desalojo->parametros[0]);
+    }
+    
 }
 
 void signal(t_pcb *proceso)
 {
     int recurso_id = get_id_recurso(proceso->contexto.motivos_desalojo->parametros[0]);
-    t_list* lista_del_recurso = list_get(LISTAS_BLOCK, recurso_id);
-    if (list_size(lista_del_recurso) > 0)
-    {
+    if(recurso_id != -1){
+        t_list* lista_del_recurso = list_get(LISTAS_BLOCK, recurso_id);
+     if (list_size(lista_del_recurso) > 0){
+
         t_pcb *proceso_a_desbloquear = list_remove(list_get(LISTAS_BLOCK, recurso_id), 0);
         cambio_de_estado(proceso_a_desbloquear->contexto.PID,"Block","Ready");
         agregar_a_lista_ready(proceso_a_desbloquear);
-    }
-    else
-    {
+
+        }
+        else{
         // sem_signal(SEMAFOROS_RECURSOS[recurso_id]);
         INSTANCIAS_RECURSOS[recurso_id]++;
+        }
+        log_info(LOGGER_KERNEL,"PID: <%d> - Signal: <%s> - Instancias: <%d>",proceso->contexto.PID,RECURSOS[recurso_id],atoi(INSTANCIAS_RECURSOS[recurso_id]));
+    }else{
+        log_info(LOGGER_KERNEL,"Se realiza Signal de proceso inexitente: <%s>",proceso->contexto.motivos_desalojo->parametros[0]);
+
     }
-    log_info(LOGGER_KERNEL,"PID: <%d> - Signal: <%s> - Instancias: <%d>",proceso->contexto.PID,RECURSOS[recurso_id],atoi(INSTANCIAS_RECURSOS[recurso_id]));
+    
 
 }
 
