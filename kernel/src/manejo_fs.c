145bac5e763b7d16ce0ad3e4681cd1cdc2309824
Andres Martin Trias <71241098+AndyTrias@users.noreply.github.com>
2023-07-28 19:11:01 -0300

    Update manejo_fs.c

--- kernel/src/manejo_fs.c
+++ kernel/src/manejo_fs.c
@@ -246,7 +246,7 @@ void f_write(t_pcb *proceso, char *nombre_archivo)
 
     long dir_fisica = strtol(proceso->contexto->motivos_desalojo->parametros[1], NULL, 10);
     int cant_bytes = atoi(proceso->contexto->motivos_desalojo->parametros[2]);
-    log_info(LOGGER_KERNEL, "PID: <%d> - Escribir Archivo: <%s> - Puntero <%d> - Dirección Memoria <%d> - Tamaño <%d>", proceso->contexto->PID, nombre_archivo, puntero, (void*) dir_fisica, cant_bytes);
+    log_info(LOGGER_KERNEL, "PID: <%d> - Escribir Archivo: <%s> - Puntero <%d> - Dirección Memoria <%p> - Tamaño <%d>", proceso->contexto->PID, nombre_archivo, puntero, (void*) dir_fisica, cant_bytes);
     log_info(LOGGER_KERNEL, "PID: <%d> - Bloqueado por: <%s>", proceso->contexto->PID, nombre_archivo);
     cambio_de_estado(proceso->contexto->PID, "Exec", "Block");
   }
